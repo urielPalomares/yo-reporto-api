257cfc8f2d77a2b2f74b478e71cae10562ddc142
Uriel Rolando Palomares Elias <uriel.palomares@kavak.com>
2023-08-01 18:34:56 -0600

    feat: pass connection variables to .env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  HOST: "localhost",
-  PORT: 8889,
-  USER: "root",
-  PASSWORD: "root",
-  DB: "yo-reporto",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  PORT: process.env.DB_PORT,
+  USER: process.env.DB_USERNAME,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
